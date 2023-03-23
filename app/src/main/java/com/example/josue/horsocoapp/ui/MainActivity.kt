ce2d9e5b5b2b5bf8282f5e57e07ee4d9ff97efb3
Josue Ruiz <josueruiz_sz@outlook.com>
2023-03-23 17:46:48 -0600

    Navigation complete

---
+++ app/src/main/java/com/example/josue/horsocoapp/ui/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.josue.horsocoapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.josue.horsocoapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
